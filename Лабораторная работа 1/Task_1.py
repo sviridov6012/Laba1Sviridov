9da8e4fe36abb5ae6268e65973f03aef8cb5f10e
sviridov6012 <160075897+sviridov6012@users.noreply.github.com>
2024-10-23 21:33:03 +0300

    Update Task_1.py

--- Лабораторная работа 1/Task_1.py
+++ Лабораторная работа 1/Task_1.py
@@ -1,7 +1,7 @@
 numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
-sum1 = sum(numbers[:4] + numbers[5:])
+sum_of_numbers = sum(numbers[:4] + numbers[5:])
 length = len(numbers)
-middle = sum1/length
+middle = sum_of_numbers / length
 numbers[4] = middle
 # TODO заменить значение пропущенного элемента средним арифметическим
 
